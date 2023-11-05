813c7c6aaec085b9d1129fe2fc35b9bd04182a2e
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-11-04 21:29:57 -0300

    Refactor code to improve image selection and API request efficiency ✨

--- helpers/geeApi.py
+++ helpers/geeApi.py
@@ -3,20 +3,51 @@ import numpy as np
 import io
 import ee
 
+# Modify this so that the system gets the list of images and saves it
+# insted of just using the first one in the list
+
+# By saving the date and the image id, it is possible to let
+# the class user choose witch one it wants to request
+
+# Doing this may save a lot of API requests
+
+# ALSO: MAKE AN API REQUEST COUNTER
+
 class PrecorsiaGee:
-    def __init__(self, dataset, rangeset, margin=50):
+    
+    def __init__(self, dataset, band_range, margin=50):
         self.dataset = dataset
-        self.rangeset = rangeset
+        self.band_range = band_range
         self.margin = margin
 
-    def init(self):
+    @staticmethod
+    def init():
         ee.Initialize()
 
-    def initLogin(self):
+    @staticmethod
+    def initLogin():
         ee.Authenticate()
         ee.Initialize()
 
-    def request(self, image, geolocation, band, band_range):
+    def list(self, geolocation, dateset):
+
+        _filter = ee.Filter.And(
+            ee.Filter.bounds(ee.Geometry.Point(geolocation, 'EPSG:4326'), self.margin),
+            ee.Filter.date(dateset[0], dateset[1])
+        )
+
+        _image_collection = ee.ImageCollection(self.dataset).filter(_filter)
+        _image_list = _image_collection.toList(0xffff)
+
+        if _image_list.size().getInfo() == 0:
+            raise Exception('No images found')
+        
+        _image_list = _image_list.map(lambda img: ee.Dictionary(
+            {'id': ee.Image(img).id(), 'time_start': ee.Image(img).get('system:time_start')})).getInfo()
+
+        return _image_list
+
+    def request(self, image, bands, geolocation):
 
         _proj = ee.Projection('EPSG:4326').atScale(10).getInfo()
         _scale_x = _proj['transform'][0]
@@ -25,7 +56,7 @@ class PrecorsiaGee:
         return {
             'assetId': self.dataset + '/' + image,
             'fileFormat': 'PNG',
-            'bandIds': [band],
+            'bandIds': bands,
             'grid': {
                 'dimensions': {
                     'width': 512,
@@ -41,27 +72,16 @@ class PrecorsiaGee:
                 },
                 'crsCode': _proj['crs']
             },
-            'visualizationOptions':  {'ranges': [{'min': band_range[0], 'max': band_range[1]}]},
+            'visualizationOptions':  {'ranges': [{'min': self.band_range[0], 'max': self.band_range[1]}]},
         }
 
-    def image(self, geolocation, dateset, band, band_range):
+    def image(self, image, bands, geolocation):
 
-        _filter = ee.Filter.And(
-            ee.Filter.bounds(geolocation, self.margin),
-            ee.Filter.date(dateset[0], dateset[1])
-        )
-
-        _image_collection = ee.imagecollection(self.dataset).filter(_filter)
-        _image_list = _image_collection.toList()
-
-        if len(_image_list) == 0:
-            raise Exception('No images found')
-
-        _image = ee.Image(_image_list[0])
-        _image_data = self.request(_image, geolocation, band, band_range)
+        _image_request = self.request(image, bands, geolocation)
+        _image_data = ee.data.getPixels(_image_request)
 
         _image_pixels = Image.open(io.BytesIO(_image_data))
         _image_p_l = _image_pixels.convert('L')
         _image_array = np.array(_image_p_l)
 
-        return _image_array 
+        return _image_array
